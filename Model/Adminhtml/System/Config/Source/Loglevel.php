7c636649f4a829922180f4a528fed503e2f3d588
Sebastian Ertner <sebastian.ertner@netresearch.de>
2017-04-13 10:27:40 +0200

    DHLVM2-67: change return types

--- Model/Adminhtml/System/Config/Source/Loglevel.php
+++ Model/Adminhtml/System/Config/Source/Loglevel.php
@@ -41,7 +41,7 @@ class Loglevel
     /**
      * Options getter
      *
-     * @return array
+     * @return mixed
      */
     public function toOptionArray()
     {
@@ -58,7 +58,7 @@ class Loglevel
     /**
      * Get options in "key-value" format
      *
-     * @return array
+     * @return mixed
      */
     public function toArray()
     {
--- Model/Adminhtml/System/Config/Source/PageSize.php
+++ Model/Adminhtml/System/Config/Source/PageSize.php
@@ -41,7 +41,7 @@ class PageSize
     /**
      * Options getter
      *
-     * @return array
+     * @return mixed
      */
     public function toOptionArray()
     {
@@ -58,7 +58,7 @@ class PageSize
     /**
      * Get options
      *
-     * @return array
+     * @return mixed
      */
     public function toArray()
     {
