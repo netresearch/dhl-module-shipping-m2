780895739f835a8ef1425d0658e21544c5b91dfc
Benjamin Heuer <benjamin.heuer@netresearch.de>
2017-06-14 15:31:26 +0200

    DHLVM2-90: add logging to webservice

--- Test/Integration/Model/Plugin/Checkout/ShippingInformationManagementPluginTest.php
+++ Test/Integration/Model/Plugin/Checkout/ShippingInformationManagementPluginTest.php
@@ -23,7 +23,7 @@
  * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
  * @link      http://www.netresearch.de/
  */
-namespace Dhl\Shipping\Model\Plugin;
+namespace Dhl\Shipping\Plugin;
 
 use \Dhl\Shipping\Model\ShippingInfo\QuoteShippingInfoRepository;
 use \Magento\Checkout\Model\ShippingInformation;
