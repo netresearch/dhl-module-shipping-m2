0937ddfd71cbab904d8b4f146b9193b281ec1f1a
Paul Siedler <paul.siedler@netresearch.de>
2017-07-12 14:58:19 +0200

    DHLVM2-110: create catalog attribute for dangerous goods

---
+++ Setup/UpgradeData.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Dhl Shipping
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to
+ * newer versions in the future.
+ *
+ * @category  Dhl
+ * @package   Dhl\Shipping\Setup
+ * @author    Paul Siedler <paul.siedler@netresearch.de>
+ * @copyright 2017 Netresearch GmbH & Co. KG
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+
+namespace Dhl\Shipping\Setup;
+
+
+use Magento\Eav\Setup\EavSetupFactory;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+    /**
+     * Eav setup factory
+     * @var EavSetupFactory
+     */
+    private $eavSetupFactory;
+
+    /**
+     * Init
+     * @param EavSetupFactory $categorySetupFactory
+     */
+    public function __construct(\Magento\Eav\Setup\EavSetupFactory $eavSetupFactory)
+    {
+        $this->eavSetupFactory = $eavSetupFactory;
+    }
+
+    public function upgrade(
+        ModuleDataSetupInterface $setup,
+        ModuleContextInterface $context
+    ) {
+        $eavSetup = $this->eavSetupFactory->create(['setup' => $setup]);
+        if ($context->getVersion() === '0.1.0') {
+            ShippingSetup::addDangerousGoodsCategoryAttribute($eavSetup);
+        }
+    }
+}
