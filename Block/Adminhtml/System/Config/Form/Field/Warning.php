aafbe36739c61a186eab068ed896c40033627a26
Max Melzer <max.melzer@moehrenzahn.de>
2018-01-30 12:24:42 +0100

    DHLVM2-187: Add Warning frontend config model, handle API_TYPE_NA

---
+++ Block/Adminhtml/System/Config/Form/Field/Warning.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Dhl Shipping
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to
+ * newer versions in the future.
+ *
+ * PHP version 7
+ *
+ * @category  Dhl
+ * @package   Dhl\Shipping
+ * @author    Max Melzer <max.melzer@netresearch.de>
+ * @copyright 2017 Netresearch GmbH & Co. KG
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+namespace Dhl\Shipping\Block\Adminhtml\System\Config\Form\Field;
+
+use Magento\Config\Block\System\Config\Form\Field;
+
+/**
+ * Config field block for simple warning messages.
+ *
+ * @category Dhl
+ * @package  Dhl\Shipping
+ * @author   Max Melzer <max.melzer@netresearch.de>
+ * @license  http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link     http://www.netresearch.de/
+ */
+class Warning extends Field
+{
+
+    /**
+     * Retrieve HTML markup for given form element
+     *
+     * @param \Magento\Framework\Data\Form\Element\AbstractElement $element
+     * @return string
+     */
+    public function render(\Magento\Framework\Data\Form\Element\AbstractElement $element)
+    {
+        $html = '<td colspan="2">'
+            . '<div class="message message-warning">'
+            . '<strong>'.$element->getLabel().'</strong>';
+        if ($comment = $element->getComment()) {
+            $html .= ': '.$comment;
+        }
+        $html .= '</div></td>';
+        
+        return $this->_decorateRowHtml($element, $html);
+    }
+}
