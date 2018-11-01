2def81808f0186d4a6d60a7aa3a1859890c91c6d
Sebastian Ertner <sebastian.ertner@netresearch.de>
2018-11-01 15:10:42 +0100

    DHLVM2-277: add backend model for validation

---
+++ Model/Adminhtml/System/Config/DropOffDays.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Dhl Shipping
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this extension to
+ * newer versions in the future.
+ *
+ * PHP version 7
+ *
+ * @package   Dhl\Shipping\Model
+ * @author    Sebastian Ertner <sebastian.ertner@netresearch.de>
+ * @copyright 2018 Netresearch GmbH & Co. KG
+ * @license   http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link      http://www.netresearch.de/
+ */
+
+namespace Dhl\Shipping\Model\Adminhtml\System\Config;
+
+use Magento\Framework\App\Config\Value;
+use Magento\Framework\Exception\LocalizedException;
+
+/**
+ * DropOffDays
+ *
+ * @package Dhl\Shipping\Model
+ * @author  Sebastian Ertner <sebastian.ertner@netresearch.de>
+ * @license http://opensource.org/licenses/osl-3.0.php Open Software License (OSL 3.0)
+ * @link    http://www.netresearch.de/
+ */
+class DropOffDays extends Value
+{
+
+
+    /**
+     * @return $this|Value
+     * @throws LocalizedException
+     */
+    public function beforeSave()
+    {
+        $noDropOffConfig = explode(',', $this->getValue());
+
+        if (count($noDropOffConfig) === 6) {
+            throw new LocalizedException(__('You need to have at least one drop off day.'));
+        }
+
+        return $this;
+    }
+
+}
